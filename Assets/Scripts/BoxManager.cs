8c413c259badbd7654d6720828ae08ae791df2b0
yyycct <yyycct02@gmail.com>
2021-10-03 18:20:07 -0700

    Revert "aa"
    This reverts commit c81dbeea015a94922252e6062ccdeb34576794d6.

--- Assets/Scripts/BoxManager.cs
+++ Assets/Scripts/BoxManager.cs
@@ -12,11 +12,10 @@ public class BoxManager : MonoBehaviour
     }
     public GameObject[] boxItems;
     public List<int> itemId = new List<int>();
-    private List<int> idList = new List<int>();
     private int cont =  0;
     void Start()
     {
-        while (itemId.Count < boxItems.Length)
+        while (itemId.Count < 9)
         {
             int rand = Random.Range(0, 9);
             if (!itemId.Contains(rand))
@@ -24,19 +23,6 @@ public class BoxManager : MonoBehaviour
                 itemId.Add(rand);
             }
         }
-        /*for (int i = 0; i < boxItems.Length; i++)
-        {
-            idList.Add(i);
-        }
-        Debug.Log(idList);
-
-        while (idList.Count > 0)
-        {
-            int rand = Random.Range(0, idList.Count);
-            itemId.Add(idList[rand]);
-            idList.RemoveAt(idList[rand]);
-        }*/
-        Debug.Log(itemId);
     }
 
     // Update is called once per frame
